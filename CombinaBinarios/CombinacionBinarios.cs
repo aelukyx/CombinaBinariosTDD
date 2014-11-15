f437a2b34571e40b74bf6c084f593f1f9089eca3
Alexander Raúl Cabrera Guevara <arcabrerag@outlook.com>
2014-11-15 10:27:07 -0500

    CombinaBinarios.Vs.1.0
    Combina binarios , es una versión parecida a piensa en binario,
    completado en tiempo de ocio. :school:

---
+++ CombinaBinarios/CombinacionBinarios.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace CombinaBinarios
+{
+    public class CombinacionBinarios
+    {
+
+        public string Combinaciones(int number)
+        {
+            string result = "";
+
+            List<string> list = new List<string>();
+
+            Regex rgx = new Regex("11");
+
+            int numeroCombinaciones = Convert.ToInt32(Math.Pow(2, number));
+
+            int num = Convert.ToInt32(Convert.ToString(3, 2));
+
+            for (int i = 0; i < numeroCombinaciones; i++)
+            {
+                string binarioUnitario = Convert.ToString(i, 2).PadLeft(number, '0');
+
+                if (rgx.IsMatch(binarioUnitario) == false)
+                {
+                    list.Add(binarioUnitario);
+                }
+            }
+
+            for (int k = 0; k < list.Count; k++)
+            {
+                result = result + list[k] + " " ;
+            }
+
+            return result;
+
+        }
+    }
+}
+
+
+
---
+++ TesteaCombinaBinarios/TesteaCombinacion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using CombinaBinarios;
+
+namespace TesteaCombinaBinarios
+{
+    [TestFixture]
+    public class TesteaCombinacion
+    {
+
+        private CombinacionBinarios combinacionBinarios { get; set; }
+
+        [SetUp]
+        public void InstaciasAntesDeCadaTest()
+        {
+            // Arrange
+            combinacionBinarios = new CombinacionBinarios();
+        }
+
+        [Test]
+        public void Combinaciones3()
+        {
+            // Act
+            var result = combinacionBinarios.Combinaciones(3);
+
+            // Assert
+            Assert.AreEqual("000 001 010 100 101 ", result);
+        }
+
+        [Test]
+        public void Combinaciones4()
+        {
+            // Act
+            var result = combinacionBinarios.Combinaciones(4);
+
+            // Assert
+            Assert.AreEqual("0000 0001 0010 0100 0101 1000 1001 1010 ", result);
+        }
+
+    }
+}
